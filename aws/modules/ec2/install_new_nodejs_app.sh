53a98845d804577ff660b7dce10b2136d6fb4d36
Duyên Lê <aleabulaziz@gmail.com>
2024-01-08 18:51:36 +0700

    build first gitlab ci to build nodejs app, run terraform with user data script

--- aws/modules/ec2/install_new_nodejs_app.sh
+++ aws/modules/ec2/install_new_nodejs_app.sh
@@ -7,16 +7,13 @@ curl -fsSL https://get.docker.com | sudo sh
 # Add the current user to the Docker group (only needed if you want to run Docker commands without sudo)
 sudo usermod -aG docker ubuntu 
 newgrp docker
+# this only takes effect in a new shell settion
 
-git clone -b feature1 --single-branch https://gitlab.com/smartosc3/roadmap.git /home/ubuntu/roadmap
+git clone -b feature2 --single-branch https://gitlab.com/smartosc3/roadmap.git /home/ubuntu/roadmap
 
-if [ -d "/home/ubuntu/roadmap" ]; then
-    cd /home/ubuntu/roadmap
-else
-    echo "/home/ubuntu/roadmap does not exist"
-    exit 1
-fi
-sudo docker build -t roadmap .
+cd /home/ubuntu/roadmap/app
+
+sudo docker build -t app .
 
 # Run the Docker container
-sudo docker run -dp 3000:3000 roadmap
+sudo docker run -dp 3000:3000 app
