2aeaaa9344bfe6475368d5abf45dbb5427314e7a
Duyên Lê <aleabulaziz@gmail.com>
2024-01-09 16:32:59 +0700

    updata gitlab ci to run terraform

--- aws/modules/ec2/outputs.tf
+++ aws/modules/ec2/outputs.tf
@@ -1,3 +1,8 @@
 output "ec2_security_group_id" {
   value = aws_security_group.ec2_security_group.id
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.fuck1.public_ip
+  description = "The public IP address of the EC2 instance."
+}
